7c8a7aeb0579d44057610a8bba829c654127a663
alif-786 <alipkamate1@gmail.com>
2024-05-27 15:55:37 +0530

    project vercel upload

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6108fba4986abc61"; }
+/******/ 		__webpack_require__.h = function() { return "0b8cb7dffc92a731"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,16 @@
 export default {
     logo: <span>python 🐍</span>,
     project: {
-      link: 'https://github.com/shuding/nextra'
+      link: 'https://github.com/alif-786/python_notes/tree/main'
+    },
+    footer: {
+      text: (
+        <span>
+          all rights reserver
+          ALIF {new Date().getFullYear()} ©{' '}
+          
+          .
+        </span>
+      )
     }
-    // ... other theme options
   }
